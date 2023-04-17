854fb345e7dbe3076a1251eabccbeb5b7a04ad22
Marcello Kazuo <marcellokazuo@gmail.com>
2023-04-17 16:39:03 -0300

    Phone dial

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -6,51 +6,147 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity 
-          onPress={this.onPress}
-          style={{
-            height: 50,
-            width: 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#ccc',
-            padding: 10,
-            borderWidth: 1,
-            borderColor: 'black',
-          }} 
-        >
-          <Text>1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          onPress={this.onPress}
-          style={{
-            height: 50,
-            width: 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#ccc',
-            padding: 10,
-            borderWidth: 1,
-            borderColor: 'black',
-          }} 
-        >
-          <Text>2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          onPress={this.onPress}
-          style={{
-            height: 50,
-            width: 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#ccc',
-            padding: 10,
-            borderWidth: 1,
-            borderColor: 'black',
-          }} 
-        >
-          <Text>3</Text>
-        </TouchableOpacity>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>3</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>6</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={this.onPress}
+            style={{
+              height: 50,
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ccc',
+              padding: 10,
+              borderWidth: 1,
+              borderColor: 'black',
+            }} 
+          >
+            <Text>9</Text>
+          </TouchableOpacity>
+        </View>
         <StatusBar style="auto" />
       </View>
     );  
