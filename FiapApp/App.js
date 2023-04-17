4a2df9950424b68aa39530ba1a910051c403e6ef
Marcello Kazuo <marcellokazuo@gmail.com>
2023-04-17 14:31:26 -0300

    AppClima v1

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>São Paulo</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24º</Text>
+
+        <TextInput 
+          autoCorrect='false'
+          placeholder='Search and City'
+          placeholderTextColor='white'
+          style={styles.textInput}
+        />
         <StatusBar style="auto" />
       </View>
     );  
@@ -19,5 +28,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Rules' : 'Roboto',
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  mediumText: { 
+    fontSize: 26,
+  },
+  largeText: { 
+    fontSize: 44,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
   },
 });
