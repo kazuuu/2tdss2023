1ee8150e3d9e4655027695f91dc364597c66b01b
Marcello Kazuo <marcellokazuo@gmail.com>
2023-04-24 17:43:38 -0300

    flex 1

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{ width: 150, height: 150, backgroundColor: 'steelblue'}} />
         <StatusBar style="auto" />
       </View>
-    );  
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
