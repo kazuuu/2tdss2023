9e40e23ee7ea8f26392046f061ac64092bc7b63e
Marcello Kazuo <marcellokazuo@gmail.com>
2023-04-25 14:24:14 -0300

    flex 1

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -6,9 +6,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{ width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{ height: 50, width: 50, backgroundColor: 'powderblue'}} />
+        <View style={{ height: 50, width: 50, backgroundColor: 'skyblue'}} />
+        <View style={{ height: 50, width: 50, backgroundColor: 'steelblue'}} />
         <StatusBar style="auto" />
       </View>
     )
