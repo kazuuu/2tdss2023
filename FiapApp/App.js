ccb8d5e3e94e2218878c02c4deec001190a16711
Marcello Kazuo <marcellokazuo@gmail.com>
2023-04-25 15:26:39 -0300

    flex 3 dimensoes flex

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -5,10 +5,10 @@ import { View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ height: '100%', width: '100%' }}>
-        <View style={{ height: '10%', width: '10%', backgroundColor: 'red'}} />
-        <View style={{ height: '10%', width: '10%', backgroundColor: 'green'}} />
-        <View style={{ height: '10%', width: '10%', backgroundColor: 'blue'}} />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: 'red'}} />
+        <View style={{ flex: 2, backgroundColor: 'green'}} />
+        <View style={{ flex: 3, backgroundColor: 'blue'}} />
         <StatusBar style="auto" />
       </View>
     )
