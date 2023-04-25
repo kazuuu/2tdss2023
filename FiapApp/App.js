ff694003891dedf500154cdceae6e28019a59e01
Marcello Kazuo <marcellokazuo@gmail.com>
2023-04-25 15:14:44 -0300

    cosmetic fix

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -5,7 +5,7 @@ import { View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ height: '100%', width: '100%'}}>
+      <View style={{ height: '100%', width: '100%' }}>
         <View style={{ height: '10%', width: '10%', backgroundColor: 'powderblue'}} />
         <View style={{ height: '10%', width: '10%', backgroundColor: 'skyblue'}} />
         <View style={{ height: '10%', width: '10%', backgroundColor: 'steelblue'}} />
