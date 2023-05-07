1ed996c1fc08069eabead1c7c16507583096a801
Marcello Kazuo <marcellokazuo@gmail.com>
2023-05-07 15:36:23 -0300

    function component use state

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -4,7 +4,6 @@ import { Button, Text, View } from 'react-native';
 export default function App() {
   const [contador, setContador] = useState(0);
 
-  useEffect
   return (
     <View style={{
       flex: 1,
