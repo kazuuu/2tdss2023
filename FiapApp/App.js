0a360b4d1d81600132526b3ecb5bb8157d1b0282
Marcello Kazuo <marcellokazuo@gmail.com>
2023-05-07 17:22:12 -0300

    Function Component 7 hooks useEffect StateChange

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -3,14 +3,11 @@ import { Button, Text, View } from 'react-native';
 
 export default function App() {
   const [contador, setContador] = useState(0);
+  const [outroValor, setOutroValor] = useState(0);
 
   useEffect(() => {
     alert('Componente renderizado!');
-
-    return () => {
-      console.log('Componente desmontado');
-    }
-  }, [])
+  }, [outroValor])
   
   return (
     <View style={{
@@ -19,7 +16,9 @@ export default function App() {
       justifyContent: 'center',
     }}>
       <Text>Nº de cliques: {contador}</Text>
+      <Text>Outro Valor: {outroValor}</Text>
       <Button title="Clique Aqui" onPress={() => setContador(contador + 1)} />
+      <Button title="outroValor" onPress={() => setOutroValor(outroValor + 1)} />
     </View>
   );
 }
