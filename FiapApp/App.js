cb94a35906a748d3c32861b6072695fccecd61ca
Marcello Kazuo <marcellokazuo@gmail.com>
2023-08-14 20:22:47 -0300

    Components 3 part 3 Flat List

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,39 +1,48 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 
 const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+  const [userList, setuserList] = useState([
+    { id: 1, nome: 'Marcello' },
+    { id: 2, nome: 'Joao' },
+    { id: 3, nome: 'Maria' },
+    { id: 4, nome: 'Celia' },
+    { id: 5, nome: 'Celia' },
+    { id: 6, nome: 'Celia' },
+    { id: 7, nome: 'Celia' },
+    { id: 8, nome: 'Celia' },
+    { id: 9, nome: 'Celia' },
+    { id: 10, nome: 'Celia' },
+    { id: 10, nome: 'Celia' },
+    { id: 10, nome: 'Celia' },
+    { id: 10, nome: 'Celia' },
+    { id: 10, nome: 'Celia' },
+    { id: 11, nome: 'Celia' },
+    { id: 12, nome: 'TESTE' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Modal
-        animationType=''
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={[styles.container, styles.bgDark ]}>
-          <Text>MODAL</Text>
-          <Pressable
-            style={styles.botao}
-            onPress={() => setModalVisible(false)}
-          >
-            <Text style={styles.botaoFonte}>FECHAR</Text>
-          </Pressable>
-
-        </View>
-      </Modal>
-
-      <Text>teste</Text>
-      <Pressable
-        style={styles.botao}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.botaoFonte}>Abrir</Text>
-      </Pressable>
-
+      <StatusBar 
+        backgroundColor='green'
+        barStyle='default'
+        hidden={true}
+      />
+      <Text style={styles.font4}>
+        Lista de Usuarios:
+      </Text>
+      <FlatList
+        style={styles.list}
+        data ={userList}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.font3}>{item.id} - {item.nome}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 } 
@@ -41,21 +50,36 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   bgDark: {
     backgroundColor: '#ccc',
   },
-  botao: {
-    backgroundColor: 'red',
+  list: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  item: {
+    flexDirection: 'row',
+    backgroundColor: '#ccc',
     padding: 10,
     borderRadius: 10,
+    marginBottom: 10,
   },
-  botaoFonte: {
-    fontSize: 20
-  }
+  font1: {
+    fontSize: 12
+  },
+  font2: {
+    fontSize: 22
+  },
+  font3: {
+    fontSize: 32
+  },
+  font4: {
+    fontSize: 42
+  },
 
 });
 
