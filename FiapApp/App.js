e88e6f7c1c0d60815a45086d115d151ea9dc082b
Marcello Kazuo <marcellokazuo@gmail.com>
2023-08-14 19:42:41 -0300

    Components 3 part 1 Modal

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );  
-  }
-}
+
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType=''
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={[styles.container, styles.bgDark ]}>
+          <Text>MODAL</Text>
+          <Pressable
+            style={styles.botao}
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={styles.botaoFonte}>FECHAR</Text>
+          </Pressable>
+
+        </View>
+      </Modal>
+
+      <Text>teste</Text>
+      <Pressable
+        style={styles.botao}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.botaoFonte}>Abrir</Text>
+      </Pressable>
+
+    </View>
+  );
+} 
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +45,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bgDark: {
+    backgroundColor: '#ccc',
+  },
+  botao: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 10,
+  },
+  botaoFonte: {
+    fontSize: 20
+  }
+
 });
+
+export default App;
