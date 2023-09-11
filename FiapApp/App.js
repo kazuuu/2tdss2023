a81d9283fbfa56f2e4d10e7d5c691cda3d8165ed
Marcello Kazuo <marcellokazuo@gmail.com>
2023-09-11 20:33:54 -0300

    Aula Contexto

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import { ThemeContext, themes } from './src/context/ThemeContext'
+import ThemeToggler from './src/components/Toggler';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );  
-  }
+
+export default function App() {
+  const [theme, setTheme] = useState(themes.light)
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <Header />
+      <ThemeToggler />
+    </ThemeContext.Provider>
+  );  
 }
 
 const styles = StyleSheet.create({
