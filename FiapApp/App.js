a6667b7b78a25a053285cb4fd6784b50d5962eb1
Marcello Kazuo <marcellokazuo@gmail.com>
2023-09-18 20:57:38 -0300

    firebase real time database

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native';
+import { db } from './firebase';
+import { set, ref, onValue, remove, update, push, child } from "firebase/database";
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    onValue(ref(db, `/items`), (snapshot) => {
+      setData([]);
+      const data = snapshot.val();
+      if (data !== null) {
+        Object.values(data).map((item) => {
+          setData((oldArray) => [...oldArray, item]);
+        });
+      }
+    });
+
+  }, []);
+
+  const adicionar = (value) => {
+    const id = push(child(ref(db), 'items')).key
+    set(ref(db, `/items/${id}`), {
+      id: id,
+      descricao: value,
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Pressable style={styles.button} onPress={() => adicionar("TESTE 2")}>
+        <Text>BOTAO!</Text>
+      </Pressable>
+
+      <FlatList
+        data={data}
+        keyExtractor={({id}) => id}
+        renderItem={({item}) => {
+          console.log("data", data);
+          return (
+          <Text style={{
+            color: '000'
+          }}>{item.descricao}</Text>
+        )}
+        } />
+
       <StatusBar style="auto" />
     </View>
   );  
---
+++ FiapApp/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFfshbl-1xfDHVQ5Xh5_uBD9FAGvYln9s",
+  authDomain: "fiap2023-d8cb7.firebaseapp.com",
+  databaseURL: "https://fiap2023-d8cb7-default-rtdb.firebaseio.com",
+  projectId: "fiap2023-d8cb7",
+  storageBucket: "fiap2023-d8cb7.appspot.com",
+  messagingSenderId: "276369474197",
+  appId: "1:276369474197:web:527e8916c8e0132b49519c",
+  measurementId: "G-CCY0RXGZPP"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
+export const db = getDatabase(app);
