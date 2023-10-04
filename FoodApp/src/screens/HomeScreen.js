1ebc1901bcca9c4e3fc312a137adad718b573f68
Marcello Kazuo <marcellokazuo@gmail.com>
2023-10-04 12:38:37 -0300

    add item

--- FoodApp/src/screens/HomeScreen.js
+++ FoodApp/src/screens/HomeScreen.js
@@ -22,40 +22,8 @@ const HomeScreen = () => {
   const {pedidos, setPedidos} = useContext(PedidosContext);
 
   useEffect(() => { 
-    // console.log("home init", pedidos);
-
-    // onValue(ref(db, '/pedidos'), (snapshot) => {
-    //   console.log("home onValue snapshot", snapshot.val());
-    //   const data = snapshot.val();
-    //   if (data != null) {
-    //     Object.values(data).map(async (item) => {
-    //       console.log("home onValue addItem 1", item);
-    //       await setPedidos((oldArray) => [...oldArray, item])
-    //       console.log("home onValue addItem 2", pedidos);
-    //     });
-    //   }
-    // });
 
   }, []);
-
-  //   // declare the data fetching function
-  //   const fetchData = async () => {
-  //     let s = await AsyncStorage.getItem('pedidos');
-  //     console.log("Pedidos 1", s)
-  //     if (s != null) {
-  //       await setPedidos(JSON.parse(s));  
-  //     }
-
-  //     console.log("Pedido 2", pedidos);   
-  //   }
-  
-  //   // call the function
-  //   fetchData()
-  //     // make sure to catch any error
-  //     .catch(console.error);
-  // }, []);
-
-
 
   const clickCategory = (categoryId) => {
     setSelectedCategoryId(categoryId);
@@ -69,42 +37,41 @@ const HomeScreen = () => {
   }
 
   const clickAdicionarFood = async (food) => {
-    // console.log("Adicionar 1", food)
-    // let s = await AsyncStorage.getItem('pedidos');
+    console.log("Adicionar 1", food)
+    let s = await AsyncStorage.getItem('pedidos');
 
-    // if (s == null) {
-    //   console.log("Adicionar 2")
-    //   await setPedidos([]);
-    // } else {
-    //   console.log("Adicionar 3", s)
-    //   await setPedidos(JSON.parse(s));
-    //   console.log("Adicionar 4", pedidos)
-    // }
+    if (s == null) {
+      console.log("Adicionar 2")
+      await setPedidos([]);
+    } else {
+      console.log("Adicionar 3", s)
+      await setPedidos(JSON.parse(s));
+      console.log("Adicionar 4", pedidos)
+    }
       
-    // console.log("Adicionar 5", pedidos)
+    console.log("Adicionar 5", pedidos)
     
-    // // Cria um id único baseado na data e hora 
-    // let idCart = Math.floor(Date.now());
+    // Cria um id único baseado na data e hora 
+    let idCart = Math.floor(Date.now());
 
-    // //*** Para adicionar um atributo em um Objeto é aconselhável utilizar o 
-    // // Spread operator (...)
-    // food = {...food, "idCart": idCart}
+    //*** Para adicionar um atributo em um Objeto é aconselhável utilizar o 
+    // Spread operator (...)
+    food = {...food, "idCart": idCart}
     
 
-    // //*** Para adicionar um item em um Array é aconselhável utilizar o 
-    // // Spread operator (...), porém se for utilizaro push precisa lembrar
-    // // de utilizar de forma sincrona com o await */
-    // //    await pedidos.push(food);
-    // // ou com spread operator:
-    // //    pedidos = [...pedidos, food];
-    // await setPedidos([...pedidos, food]);
-    // console.log("Adicionar 6", pedidos);
+    //*** Para adicionar um item em um Array é aconselhável utilizar o 
+    // Spread operator (...), porém se for utilizaro push precisa lembrar
+    // de utilizar de forma sincrona com o await */
+    //    await pedidos.push(food);
+    // ou com spread operator:
+    //    pedidos = [...pedidos, food];
+    await setPedidos([...pedidos, food]);
+    console.log("Adicionar 6", pedidos);
 
-    // // Lembrar de transformar o objeto em uma string antes de gravar
-    // // no AsyncStorage
-    // await AsyncStorage.setItem('pedidos', JSON.stringify(pedidos));
-    // alert('Adicionado')
-    
+    // Lembrar de transformar o objeto em uma string antes de gravar
+    // no AsyncStorage
+    await AsyncStorage.setItem('pedidos', JSON.stringify(pedidos));
+    alert('Adicionado')   
 
   }
 
